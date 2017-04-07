47bee20779c8b41cdc30a351c6c3e00fc077ef7c
Gonzalo Moreno <gonzalomf@healthwave.ie>
2017-04-07 15:14:16 +0100

    The Movie Database package configuration

--- config/app.php
+++ config/app.php
@@ -231,6 +231,10 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        /*
+         * Vendors Service Providers...
+         */
+        'Tmdb' => Tmdb\Laravel\Facades\Tmdb::class,
     ],
 
 ];
