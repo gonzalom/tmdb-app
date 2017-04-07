bddb1ab0213b659c11ffe9e98ee12db5546a551c
Gonzalo Moreno <gonzalomf@healthwave.ie>
2017-04-07 14:46:57 +0100

    Move User to Models

---
+++ app/Models/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+/**
+ * Class User
+ * @package App\Models
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string $remember_token
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ *
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereNameToken($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereEmailToken($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User wherePasswordToken($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereUpdatedAt($value)
+ *
+ * @mixin \Eloquent
+ * @method static \App\Models\User create($attributes)
+ */
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
