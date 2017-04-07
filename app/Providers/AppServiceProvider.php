ca91169998b8cf95aa2a5f79a8554135213f75ff
Gonzalo Moreno <gonzalomf@healthwave.ie>
2017-04-07 14:20:02 +0100

    Fix for MySQL older than the 5.7.7 release or MariaDB older than the 10.2.2 release

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        /**
+         * Fix for non utf8mb4 charset.
+         * MySQL older than the 5.7.7 release or MariaDB older than the 10.2.2 release.
+         *
+         * SQLSTATE[42000]
+         * Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes
+         *
+         * @link https://laravel.com/docs/master/migrations#indexes
+         */
+        Schema::defaultStringLength(191);
+
         if ($this->app->environment() !== 'production') {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
