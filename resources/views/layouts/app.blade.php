75997e434060b99c312b3942ad2a5107ae753ef3
Gonzalo Moreno <gonzalomf@healthwave.ie>
2017-04-10 16:34:53 +0100

    Hide header links if guest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,12 +47,14 @@
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    @if (!Auth::guest())
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav" v-cloak>
                         <li><router-link to="/app/genre">Genres</router-link></li>
                     </ul>
 
                     <search-movie></search-movie>
+                    @endif
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
